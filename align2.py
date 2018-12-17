7dd2ae03c9ce12f86dcd010cfc78d34369940727
OkeWoke <toamumbo@gmail.com>
2018-12-17 14:22:40 +1300

    altered align2 dependancy path

--- align2.py
+++ align2.py
@@ -2,8 +2,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import SeaBreeze_Objective as SBO
-import DAQT7_Objective as DAQ
+import req.SeaBreeze_Objective as SBO
+import req.DAQT7_Objective as DAQ
 import bisect
 
 Spec = SBO.DetectSpectrometer()
